f3b36a015e864d89156e31ea1d1ca5a087b562eb
Argha Mukherjee <argha.8888@gmail.com>
2025-04-05 14:50:58 +0530

    code'

--- src/main/java/org/pattern/binarysearch/findminimuminsortedarray/LeetCode153.java
+++ src/main/java/org/pattern/binarysearch/findminimuminsortedarray/LeetCode153.java
@@ -1,4 +1,20 @@
 package org.pattern.binarysearch.findminimuminsortedarray;
 
 public class LeetCode153 {
+    public int findMin(int[] arr) {
+        int l = 0;
+        int h = arr.length - 1;
+        int min = Integer.MAX_VALUE;
+        while(l<= h ){
+            int m = l+ (h-l)/2;
+            if(arr[m]<= arr[h] ){
+                min = Math.min(min,arr[m]);
+                h = m-1;
+            }else{
+                min = Math.min(min,arr[l]);
+                l = m+1;
+            }
+        }
+        return min;
+    }
 }
