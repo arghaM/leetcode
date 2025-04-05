9b78a418add2889e7d11d455d1216a53f96eff5e
Argha Mukherjee <argha.8888@gmail.com>
2025-04-05 11:40:48 +0530

    Coco eats banna code

--- src/main/java/org/pattern/binarysearch/kokoeatingbanana/LeetCode875.java
+++ src/main/java/org/pattern/binarysearch/kokoeatingbanana/LeetCode875.java
@@ -1,5 +1,35 @@
-package org.pattern.binarysearch;
+package org.pattern.binarysearch.kokoeatingbanana;
+
+import java.util.Arrays;
 
 public class LeetCode875 {
-    public static void main
+    public static int minEatingSpeed(int[] piles, int h) {
+       int max =  Arrays.stream(piles).max().getAsInt();
+       int start = 1;
+       int ans = Integer.MAX_VALUE;
+
+       while(start <= max){
+           int m = start + ((max-start)/2);
+           int localTime = 0;
+           for(int p : piles){
+              // double t = (double) p/m;
+              // localTime += Math.ceil(t);
+               localTime += (p + m - 1) / m;
+
+           }
+           if(localTime <= h){
+               ans = Math.min(ans, m);
+               max = m - 1;
+           }else{
+               start = m+1;
+           }
+       }
+       return  ans;
+
+
+    }
+    public static void main(String args[]){
+       int piles [] = {3,6,7,11};
+        System.out.println(minEatingSpeed(piles, 8));
+    }
 }
